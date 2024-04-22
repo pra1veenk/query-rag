0b74c7c71ce914eadcc38eb5f107a83807d1a180
Praveen Kumar Padegal <p.padegal@sap.com>
2024-04-22 13:33:20 +0530

    initial code commit

---
+++ code/app/services.cds
@@ -0,0 +1,2 @@
+
+using from './query-rag-ui/annotations';
---
+++ code/srv/chat-service.js
@@ -0,0 +1,123 @@
+const cds = require('@sap/cds');
+const { DELETE } = cds.ql;
+const { handleMemoryBeforeRagCall, handleMemoryAfterRagCall } = require('./memory-helper');
+
+userId = cds.env.requires["SUCCESS_FACTORS_CREDENTIALS"]["USER_ID"]
+
+const tableName = 'SAP_TISCE_DEMO_DOCUMENTCHUNK'; 
+const embeddingColumn  = 'EMBEDDING'; 
+const contentColumn = 'TEXT_CHUNK';
+
+const systemPrompt = 
+`Your task is to answer queries using the grounding data provided.\n
+`;
+
+const genericRequestPrompt = 
+'You are a chatbot. Answer the user question based only on the context, delimited by triple backticks\n ';
+;
+
+const taskCategory = {
+    "generic-query" : genericRequestPrompt
+}
+
+function getFormattedDate (timeStamp)
+{
+    const timestamp = Number(timeStamp);
+    const date = new Date(timestamp);
+    return new Intl.DateTimeFormat('en-US', {
+        timeZone: 'GMT',
+      }).format(date);
+}
+
+module.exports = function () {
+
+    this.on('getChatRagResponse', async (req) => {
+        try {
+            //request input data
+            const { conversationId, messageId, message_time, user_id, user_query } = req.data;
+            const { Conversation, Message } = this.entities;
+            const vectorplugin = await cds.connect.to("cap-llm-plugin");
+            let hrLeavePrompt = "";
+
+            let determinationPayload = [{
+                "role" : "system",
+                "content" : `${systemPrompt}`
+              }];
+
+            const userQuestion = [
+                {
+                  "role": "user",
+                  "content": `${user_query}`
+                }
+              ]
+            
+            determinationPayload.push(...userQuestion);
+            let payload = {
+                "messages": determinationPayload
+            };
+
+            const determinationResponse = await vectorplugin.getChatCompletion(payload)
+            const determinationJson = JSON.parse(determinationResponse.content);
+            const category = determinationJson?.category ;
+            
+            //handle memory before the RAG LLM call
+            const memoryContext = await handleMemoryBeforeRagCall (conversationId , messageId, message_time, user_id , user_query, Conversation, Message );
+            
+            /*Single method to perform the following :
+            - Embed the input query
+            - Perform similarity search based on the user query 
+            - Construct the prompt based on the system instruction and similarity search
+            - Call chat completion model to retrieve relevant answer to the user query
+            */
+
+            const promptCategory  = {
+                "generic-query" : genericRequestPrompt
+            }
+
+            const chatRagResponse = await vectorplugin.getRagResponse(
+                user_query,
+                tableName,
+                embeddingColumn,
+                contentColumn,
+                promptCategory[category] ,
+                memoryContext .length > 0 ? memoryContext : undefined,
+                30
+            );
+
+            //handle memory after the RAG LLM call
+            const responseTimestamp = new Date().toISOString();
+            await handleMemoryAfterRagCall (conversationId , responseTimestamp, chatRagResponse.completion, Message, Conversation);
+
+            const response = {
+                "role" : chatRagResponse.completion.role,
+                "content" : chatRagResponse.completion.content,
+                "messageTime": responseTimestamp,
+                "additionalContents": chatRagResponse.additionalContents,
+            };
+
+            return response;
+        }
+        catch (error) {
+            // Handle any errors that occur during the execution
+            console.log('Error while generating response for user query:', error);
+            throw error;
+        }
+
+    })
+
+
+    this.on('deleteChatData', async () => {
+        try {
+            const { Conversation, Message } = this.entities;
+            await DELETE.from(Conversation);
+            await DELETE.from(Message);
+            return "Success!"
+        }
+        catch (error) {
+            // Handle any errors that occur during the execution
+            console.log('Error while deleting the chat content in db:', error);
+            throw error;
+        }
+    })
+
+}
